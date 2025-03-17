ba5ca166ee159e6ecca5603e44ba86e130343a26
songbo <1152745700@qq.com>
2025-03-17 19:33:30 +0800

    初始化提交

---
+++ src/test/java/com/example/demo/SpringAiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
